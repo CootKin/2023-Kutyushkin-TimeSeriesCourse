d2a249b8bbd55dc008522f86d7f3273ffea9c4de
Dmitry Kutyushkin <dima.kdw00@gmail.com>
2023-10-09 01:06:05 +0500

    Completed part 1 of 02 Similarity search

--- practice/02 Similarity search/modules/metrics.py
+++ practice/02 Similarity search/modules/metrics.py
@@ -1,29 +1,25 @@
 import numpy as np
-
+import math
 
 def ED_distance(ts1, ts2):
-    """
-    Calculate the Euclidean distance.
-
-    Parameters
-    ----------
-    ts1 : numpy.ndarray
-        The first time series.
-
-    ts2 : numpy.ndarray
-        The second time series.
-
-    Returns
-    -------
-    ed_dist : float
-        Euclidean distance between ts1 and ts2.
-    """
-    
     ed_dist = 0
 
-    # INSERT YOUR CODE
+    for item1, item2 in zip(ts1, ts2):
+      ed_dist += (item1 - item2)**2
+    
+    return math.sqrt(ed_dist)
 
-    return ed_dist
+def arithmeticMeanDev(ts):
+  return np.sum(ts)/len(ts)
+
+
+def standartDev(ts):
+  s = 0
+  mean_square = arithmeticMeanDev(ts)**2
+  for item in ts:
+    s += item**2 - mean_square
+  
+  return math.sqrt(s / len(ts))
 
 
 def norm_ED_distance(ts1, ts2):
@@ -43,37 +39,40 @@ def norm_ED_distance(ts1, ts2):
     norm_ed_dist : float
         The normalized Euclidean distance between ts1 and ts2.
     """
-
     norm_ed_dist = 0
 
-    # INSERT YOUR CODE
+    n = len(ts1)
+    mu1 = arithmeticMeanDev(ts1)
+    mu2 = arithmeticMeanDev(ts2)
+    sigma1 = standartDev(ts1)
+    sigma2 = standartDev(ts2)
+    
+    ed = (np.dot(ts1, ts2) - n*mu1*mu2)/(n*sigma1*sigma2)
+
+    norm_ed_dist = math.sqrt(abs(2*n*(1 - ed)))
 
     return norm_ed_dist
 
 
 def DTW_distance(ts1, ts2, r=None):
-    """
-    Calculate DTW distance.
+  
+    n = len(ts1)
+    m = len(ts2)
 
-    Parameters
-    ----------
-    ts1 : numpy.ndarray
-        The first time series.
+    dtw_matrix = np.zeros((n+1, m+1))
+    dtw_matrix[:, :] = np.inf
+    dtw_matrix[0, 0] = 0
 
-    ts2 : numpy.ndarray
-        The second time series.
+    for i in range(1, n+1):
+        for j in range(max(1, i-int(np.floor(m*r))), min(m, i+int(np.floor(m*r))) + 1):
+            cost = np.square(ts1[i-1] - ts2[j-1])
+            dtw_matrix[i, j] = cost + \
+                min(dtw_matrix[i-1, j],
+                    dtw_matrix[i, j-1],
+                    dtw_matrix[i-1, j-1])
 
-    r : float
-        Warping window size.
-    
-    Returns
-    -------
-    dtw_dist : float
-        DTW distance between ts1 and ts2.
-    """
-
-    dtw_dist = 0
-
-    # INSERT YOUR CODE
+    dtw_dist = dtw_matrix[n, m]
 
     return dtw_dist
+
+    
